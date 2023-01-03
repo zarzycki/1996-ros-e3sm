4ef7775078b89cbf64a416726da545b21d83af9c
Colin Zarzycki <zarzycki@ucar.edu>
2023-01-02 22:39:07 -0500

    updates to back ensemble generation

--- ncl/sub-job.sh
+++ ncl/sub-job.sh
@@ -1,30 +1,34 @@
 #!/bin/bash -l
 
 #PBS -l nodes=1:ppn=20
-#PBS -l walltime=2:00:00
+#PBS -l walltime=12:00:00
 #PBS -A open
 #PBS -j oe
 #PBS -N abby_ncl
 
 module load parallel
 
-# use numcores = 32 for 1deg runs, 16 for high-res runs
 NUMCORES=5
 TIMESTAMP=`date +%s%N`
 COMMANDFILE=commands.${TIMESTAMP}.txt
 
-cd /storage/home/cmz5202/arp-ncl
+cd /storage/home/cmz5202/1996-ros-e3sm/ncl
      
 harr=( "h0" "h1" "h2" "h3" )
 models=( "mosart" "eam" "elm" )
+exps=( "PI" "control" "plus1K" "plus2K" "plus3K" "plus4K" )
 for ii in "${harr[@]}"
 do
   for jj in "${models[@]}"
   do
-    NCLCOMMAND="ncl -n create-ens-avg.ncl 
-        'hname = \"'${ii}'\"' 
-        'modelcomponent = \"'${jj}'\"' "
-    echo ${NCLCOMMAND} >> ${COMMANDFILE}
+    for kk in "${exps[@]}"
+    do
+      NCLCOMMAND="ncl -n create-ens-avg.ncl 
+          'hname = \"'${ii}'\"' 
+          'experiment = \"'${kk}'\"' 
+          'modelcomponent = \"'${jj}'\"' "
+      echo ${NCLCOMMAND} >> ${COMMANDFILE}
+    done
   done 
 done
 
