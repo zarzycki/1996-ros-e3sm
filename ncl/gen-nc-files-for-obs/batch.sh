6c0cc0e8ee10d67fe83d410852f4ecff04e12756
zarzycki <czarzycki@psu.edu>
2023-01-19 22:44:03 -0500

    update grid to timeseries ncl files

---
+++ ncl/gen-nc-files-for-obs/batch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export BASEPATH=/Volumes/ZARZYCKI_FLASH/arp5873/
+export SOFTPATH=/Users/cmz5202/Software/1996-ros-e3sm/
+ncl ncl_model-precip.ncl
+ncl ncl_model-tmax.ncl 'minmax="min"'
+ncl ncl_model-tmax.ncl 'minmax="max"'
+ncl ncl_obs-precip.ncl
+ncl ncl_obs-tmax.ncl 'var="tmax"'
+ncl ncl_obs-tmax.ncl 'var="tmin"'
+ncl ncl_obs-swe.ncl 'data="obs"'
+ncl ncl_obs-swe.ncl 'data="obs_snowdepth"'
+ncl ncl_obs-swe.ncl 'data="NLDAS"'
+ncl ncl_obs-swe.ncl 'data="JRA"'
+ncl ncl_obs-swe.ncl 'data="ERA5"'
